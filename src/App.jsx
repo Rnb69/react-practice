d712889eb80aacda663806955bb93ecc3af1c20d
Yash Thakur <59911586+Rnb69@users.noreply.github.com>
2025-03-05 01:26:56 +0530

    Basic Form Validation Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./components/Search/Search";
+import Form from "./components/Form/Form.jsx";
 import {accordionData} from "./untils/Content";
 import Accordion from "./components/Accordion/Accordion";
 const App = () => {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Search />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div className="accordion">
-      {accordionData.map(({title, content}) =>(
-        <Accordion title={title} content={content} />
-      ))}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="accordion">
+    //   {accordionData.map(({title, content}) =>(
+    //     <Accordion title={title} content={content} />
+    //   ))}
+    // </div>
   );
 };
 
